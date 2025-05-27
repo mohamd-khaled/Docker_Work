3fcec1fd9570f9be38fc040966730457de9f3cd3
mohamed khaled <mohamedkhaled056@gmail.com>
2025-05-27 10:40:12 +0300

    Adding SSH project

---
+++ SSH/create_cont.sh
@@ -0,0 +1,5 @@
+docker build -t ssh-server:v1 ./SSH-server/Dockerfile
+docker build -t ssh-client:v1 ./SSH-client/Dockerfile
+
+docker container run -it --name ssh-server ssh-server:v1
+docker container run -it --name ssh-client ssh-client:v1
